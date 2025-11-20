6a6768b4e5e88e682fa0341653e2c1d0b0a132f5
Fadel Akram <af8356207@gmail.com>
2025-11-20 14:54:25 +0100

    cours en ligne extension that extends coursDecorator abstract class

--- src/main/java/com/polytech/tp/CoursEnLigne.java
+++ src/main/java/com/polytech/tp/CoursEnLigne.java
@@ -1,21 +1,24 @@
 package com.polytech.tp;
 
+import com.polytech.tp.interfaces.ICours;
+
 public class CoursEnLigne extends CoursDecorator {
-    CoursEnLigne(ICours cours) {
+    public CoursEnLigne(ICours cours) {
         //TODO: Implémenter le reste ...
+        super(cours);
     }
 
     @Override
     public String getDescription() {
         // TODO Auto-generated method stub
         
-        return null;
+        return "" +  super.getDescription() + "(En ligne)";
     }
 
     @Override
     public double getDuree() {
         // TODO Auto-generated method stub
         
-        return 0;
+        return super.getDuree();
     };
 }
