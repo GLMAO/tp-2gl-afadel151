3700d725d1016a01f4e014023c2743b83deef68b
Fadel Akram <af8356207@gmail.com>
2025-11-20 14:52:54 +0100

    Concrete builder

--- src/main/java/com/polytech/tp/CoursBuilder.java
+++ src/main/java/com/polytech/tp/CoursBuilder.java
@@ -1,14 +1,74 @@
 package com.polytech.tp;
 
-public class CoursBuilder {
+import com.polytech.tp.interfaces.CourseBuilderInterface;
+
+public class CoursBuilder implements CourseBuilderInterface {
     // TODO: Implémenter le pattern Builder
+
+    private Cours cours;
     
+    CoursBuilder() {
+        this.reset();
+    }
     
-    public CoursBuilder setMatiere(String matiere) { return this; }
-    public CoursBuilder setEnseignant(String enseignant) { return this; }
+    @Override
+    public void reset() {
+        this.cours = new Cours();
+    }
     
+    @Override
+    public CourseBuilderInterface setMatiere(String matiere) {
+        this.cours.setMatiere(matiere);
+        return this;
+    }
+
+    @Override
+    public CourseBuilderInterface setEnseignant(String enseignant) {
+        this.cours.setEnseignant(enseignant);
+        return this;
+    }
+
+    @Override
+    public CourseBuilderInterface setSalle(String salle)
+    {
+        this.cours.setSalle(salle);
+        return this;
+    };
+
+    @Override
+    public CourseBuilderInterface setHoraire(String horaire){
+        this.cours.setHeureDebut(horaire);
+        return this;
+    };
+
+    @Override
+    public CourseBuilderInterface setDate(String date){
+        this.cours.setDate(date);
+        return this;
+    };
+    @Override
+    public CourseBuilderInterface setEstOptionnel(boolean estOptionnel)
+    {
+        this.cours.setEstOptionnel(estOptionnel);
+        return this;
+    };
+    @Override
+    public CourseBuilderInterface setNiveau(String niveau)
+    {
+        this.cours.setNiveau(niveau);
+        return this;
+    };
+    @Override
+    public CourseBuilderInterface setNecessiteProjecteur(boolean necessiteProjecteur)
+    {
+        this.cours.setNecessiteProjecteur(necessiteProjecteur);
+        return this;
+    };
     
+    @Override
     public Cours build() {
-        return null;
+        Cours builtCours = this.cours;
+        this.reset();
+        return builtCours;
     }
 }
