572c527d8850440c74e47e2acee471fc69148452
Fadel Akram <af8356207@gmail.com>
2025-11-20 14:55:24 +0100

    implementing the Observer pattern Subject class methods

--- src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -1,25 +1,49 @@
 package com.polytech.tp;
-
+import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GestionnaireEmploiDuTemps {
+import com.polytech.tp.interfaces.ICours;
+import com.polytech.tp.interfaces.Observer;
+import com.polytech.tp.interfaces.Subject;
+
+public class GestionnaireEmploiDuTemps implements Subject {
+
+    private List<Observer> observers = new ArrayList<>();
     private List<ICours> listeCours = new ArrayList<>();
+    
+    @Override
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+    @Override
+    public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+    @Override
+    public void notifyObservers(String message) 
+    {
+        observers.forEach(o -> o.update(message));
+    }
 
     public void ajouterCours(ICours cours) {
         this.listeCours.add(cours);
         System.out.println("Nouveau cours ajouté : " + cours.getDescription());
         // TODO: C'est ici qu'il faudrait notifier les étudiants (Observer pattern)
+        this.notifyObservers("Nouveau cours ajouté : " + cours.getDescription());
     }
 
     public void modifierCours(ICours cours, String message) {
         // Logique de modification...
         System.out.println("Cours modifié : " + message);
         // TODO: Notifier les observateurs ici aussi
+        this.notifyObservers("Cours modifié : " + message);
     }
+
+
 
     public void setChangement(String string) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setChangement'");
+        this.notifyObservers(string);
     }
 }
