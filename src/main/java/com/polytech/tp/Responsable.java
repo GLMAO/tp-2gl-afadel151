d22557e3b0fc9edd0f8bdab2629536c6fef032ee
Fadel Akram <af8356207@gmail.com>
2025-11-20 14:55:39 +0100

    another concrete observer

--- src/main/java/com/polytech/tp/Responsable.java
+++ src/main/java/com/polytech/tp/Responsable.java
@@ -1,14 +1,21 @@
 package com.polytech.tp;
 
-public class Responsable {
-    private String nom;
+import java.beans.PropertyChangeEvent;
+
+import com.polytech.tp.interfaces.Observer;
+
+public class Responsable implements Observer{
+    private final String nom;
 
     public Responsable(String nom) {
         this.nom = nom;
     }
 
-    
+
+    @Override
     public void update(String message) {
         System.out.println("Notification pour le responsable " + nom + " : " + message);
     }
+
+    
 }
