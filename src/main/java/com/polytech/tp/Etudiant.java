c073b2212ace0f244bca2e657670cdc9abc9cf41
Fadel Akram <af8356207@gmail.com>
2025-11-20 14:54:49 +0100

    implementing observer method (update)

--- src/main/java/com/polytech/tp/Etudiant.java
+++ src/main/java/com/polytech/tp/Etudiant.java
@@ -1,14 +1,20 @@
 package com.polytech.tp;
 
-public class Etudiant {
+
+
+import com.polytech.tp.interfaces.Observer;
+
+public class Etudiant implements  Observer {
     private String nom;
 
     public Etudiant(String nom) {
         this.nom = nom;
     }
 
-    
+    @Override
     public void update(String message) {
-        System.out.println("Notification pour l'étudiant " + nom + " : " + message);
+        System.out.println("Étudiant " + nom + " a reçu la notification : " + message);
     }
+
+
 }
